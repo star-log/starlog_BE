05a92dade6fc7b6e2500957f7c0cfd9f0c1d21ef
kn4tti <gaden119000@gmail.com>
2026-06-01 22:42:43 +0900

    feat: FortuneController 추가

---
+++ src/main/java/com/starlog_be/fortune/FortuneController.java
@@ -0,0 +1,10 @@
+package com.starlog_be.fortune;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class FortuneController {
+
+}
