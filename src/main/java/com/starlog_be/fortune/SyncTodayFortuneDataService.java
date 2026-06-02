77f1cbfa4d0d78ec826bf588057ba5ddd1e9f622
kn4tti <gaden119000@gmail.com>
2026-06-02 11:16:16 +0900

    refactor: 사용하지 않는 import 제거

--- src/main/java/com/starlog_be/fortune/SyncTodayFortuneDataService.java
+++ src/main/java/com/starlog_be/fortune/SyncTodayFortuneDataService.java
@@ -5,7 +5,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
