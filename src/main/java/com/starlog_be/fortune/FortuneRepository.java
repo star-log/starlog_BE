889ba3b57f99d758eb57a510e2909be5e40f615d
kn4tti <gaden119000@gmail.com>
2026-06-03 22:50:05 +0900

    feature: Repository 어노테이션 추가

--- src/main/java/com/starlog_be/fortune/FortuneRepository.java
+++ src/main/java/com/starlog_be/fortune/FortuneRepository.java
@@ -1,6 +1,8 @@
 package com.starlog_be.fortune;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FortuneRepository extends JpaRepository<Fortune, Long> {
 }
