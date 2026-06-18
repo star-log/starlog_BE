0006ff731652c89e8e31edf7196127185c42367c
kn4tti <gaden119000@gmail.com>
2026-06-18 19:14:45 +0900

    feature: EnableScheduling 어노테이션 추가

--- src/main/java/com/starlog_be/StarlogBeApplication.java
+++ src/main/java/com/starlog_be/StarlogBeApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @ConfigurationPropertiesScan
 public class StarlogBeApplication {
 
