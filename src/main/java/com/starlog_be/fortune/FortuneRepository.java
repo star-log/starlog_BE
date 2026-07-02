ce7935f3833031a4490e9b73ee37b6dc1b33911c
kn4tti <gaden119000@gmail.com>
2026-07-02 20:18:56 +0900

    feat: findByid 추가

--- src/main/java/com/starlog_be/fortune/FortuneRepository.java
+++ src/main/java/com/starlog_be/fortune/FortuneRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 @Repository
 public interface FortuneRepository extends JpaRepository<Fortune, Long> {
     public List<Fortune> findByDate(LocalDate now);
+
+    public Fortune findByid(Long id);
 }
