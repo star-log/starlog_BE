6e6547019ca78305b11ace4f1649397f9154c779
kn4tti <gaden119000@gmail.com>
2026-07-02 19:53:03 +0900

    feat: queryFortuneDetail 메서드 및 QueryFortuneDetailService 추가

--- src/main/java/com/starlog_be/fortune/FortuneController.java
+++ src/main/java/com/starlog_be/fortune/FortuneController.java
@@ -1,6 +1,8 @@
 package com.starlog_be.fortune;
 
+import com.starlog_be.fortune.dto.QueryFortuneDetailResponse;
 import com.starlog_be.fortune.dto.QueryFortunesResponse;
+import com.starlog_be.fortune.service.QueryFortuneDetailService;
 import com.starlog_be.fortune.service.QueryFortunesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +14,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FortuneController {
     private final QueryFortunesService queryFortunesService;
+    private final QueryFortuneDetailService queryFortuneDetailService;
 
     @GetMapping
     public List<QueryFortunesResponse> queryFortunes() {
         return queryFortunesService.execute();
+    }
+
+    @GetMapping("/{id}")
+    public QueryFortuneDetailResponse queryFortuneDetail() {
+        return queryFortuneDetailService.execute();
     }
 }
---
+++ src/main/java/com/starlog_be/fortune/service/QueryFortuneDetailService.java
@@ -0,0 +1,11 @@
+package com.starlog_be.fortune.service;
+
+import com.starlog_be.fortune.dto.QueryFortuneDetailResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class QueryFortuneDetailService {
+    public QueryFortuneDetailResponse execute() {
+        return null;
+    }
+}
